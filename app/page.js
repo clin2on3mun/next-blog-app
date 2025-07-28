ee5c27d7b8507f1b1af5dcdd4a6b74082546e684
clin2on3mun <munanaclinton@gmail.com>
2025-07-28 20:49:11 +0200

    Add styles

--- app/page.js
+++ app/page.js
@@ -2,11 +2,12 @@
 import BlogList from "../components/BlogList";
 import Footer from "../components/Footer";
 import { Header } from "../components/Header";
-import Image from "next/image";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
     <>
+      <ToastContainer theme="dark"/>
       <Header/>
       <BlogList/>
       <Footer/>
